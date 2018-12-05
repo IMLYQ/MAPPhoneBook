851447e5d4773d19c002e3a9bb044e8c158ca04a
yongqing liang <1451468553@qq.com>
2018-12-05 19:44:32 +0800

    创建Person、PhoneNumber实体

--- aspnet-core/src/MAPPhoneBook.EntityFrameworkCore/Migrations/20181205114016_Add_NewPersonAndNumberEntity.Designer.cs
+++ aspnet-core/src/MAPPhoneBook.EntityFrameworkCore/Migrations/20181205114016_Add_NewPersonAndNumberEntity.Designer.cs
@@ -4,14 +4,16 @@ using MAPPhoneBook.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MAPPhoneBook.Migrations
 {
     [DbContext(typeof(MAPPhoneBookDbContext))]
-    partial class MAPPhoneBookDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181205114016_Add_NewPersonAndNumberEntity")]
+    partial class Add_NewPersonAndNumberEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
